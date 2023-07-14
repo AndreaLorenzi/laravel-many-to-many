5b336ebf93b8fe58b41c1fde4f5c9699fa1ebcd7
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-14 16:07:38 +0200

    add create,show,index,show of technology, fix crud

--- app/Http/Controllers/Admin/TecnologyController.php
+++ app/Http/Controllers/Admin/TecnologyController.php
@@ -3,83 +3,89 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
-class TecnologyController extends Controller
+class TechnologyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $validations = [
+        "name"            => "required|string|max:50",
+        "description"     => "required|string|max:5000",
+    ];
+
+    private $validation_messages = [
+        'required'  => 'Il campo :attribute è obbligatorio',
+        'min'       => 'Il campo :attribute deve avere almeno :min caratteri',
+        'max'       => 'Il campo :attribute non può superare i :max caratteri',
+        'url'       => 'Il campo deve essere un url valido',
+        'exists'    => 'Valore non valido'
+    ];
+
     public function index()
     {
-        //
+        $technologies = Technology::all();
+
+        return view('admin.technologies.index', compact('technologies'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        return view('admin.technologies.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        $request->validate($this->validations, $this->validation_messages);
+
+        $data = $request->all();
+
+        // salvare i dati nel db
+        $newTechnology = new Technology();
+
+        $newTechnology->name          = $data['name'];
+        $newTechnology->description   = $data['description'];
+
+        $newTechnology->save();
+
+        // rotta di tipo get
+        return to_route('admin.technology.show', ['technology' => $newTechnology]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(Technology $technology)
     {
-        //
+        return view('admin.technologies.show', compact('technology'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(Technology $technology)
     {
-        //
+        return view('admin.technologies.edit', compact('technology'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+
+    public function update(Request $request, Technology $technology)
     {
-        //
+        // validare i dati del form
+        $request->validate($this->validations, $this->validation_messages);
+
+        $data = $request->all();
+
+        // aggiornare i dati nel db
+        $technology->name         = $data['name'];
+        $technology->description  = $data['description'];
+
+        $technology->update();
+
+        // rotta di tipo get
+        return to_route('admin.technology.show', ['technology' => $technology->id]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(Technology $technology)
     {
-        //
+        $technology->projects()->detach();
+        $technology->delete();
+
+        return to_route('admin.technology.index')->with('delete_success', $technology);
     }
 }
