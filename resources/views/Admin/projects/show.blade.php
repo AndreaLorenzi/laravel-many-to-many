9ab3ffb4522f94ded92fe1651b5df9bf6f998a83
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-14 16:21:09 +0200

    add checkbox tech.anf fix

--- resources/views/Admin/projects/show.blade.php
+++ resources/views/Admin/projects/show.blade.php
@@ -10,7 +10,8 @@
                 <th scope="col">Last Update</th>
                 <th scope="col">Collaborators</th>
                 <th scope="col">Description</th>
-                <th scope="col">Languages</th>
+                <th scope="col">Technologies</th>
+                <th scope="col">Type</th>
                 <th scope="col">Link</th>
             </tr>
         </thead>
@@ -22,12 +23,18 @@
                 <td>{{ $project->last_update }}</td>
                 <td>{{ $project->collaborators }}</td>
                 <td>{{ $project->description }}</td>
-                <td>{{ $project->languages }}</td>
+                <td>
+                    @foreach ($project->technologies as $technology)
+                        <a
+                            href="{{ route('admin.technology.show', ['technology' => $technology]) }}">{{ $technology->name }}</a>
+                    @endforeach
+                </td>
                 <td><a href="{{ route('admin.type.show', ['type' => $project->type]) }}">{{ $project->type->name }}</a></td>
                 <td><a href="{{ $project->link_github }}">Link</a></td>
-
-
             </tr>
         </tbody>
     </table>
+    <div class="container">
+        <img class="w-100" src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->title }}">
+    </div>
 @endsection
